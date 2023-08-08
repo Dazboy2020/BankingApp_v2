259c57cf33b594ee42d9aa7584498e402bdef77a
Dazboy2020 <darrendowlut@tutanota.com>
2023-08-08 14:02:11 +0200

    transfer section setting state and styling fixed

--- src/components/Outline-Card/OutlineCard3.js
+++ src/components/Outline-Card/OutlineCard3.js
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 // import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 // import Typography from '@mui/material/Typography';
 import { TextField, MenuItem, Typography, FormControl } from '@mui/material';
 import { useState } from 'react';
@@ -28,6 +28,7 @@ export default function BasicCardTransaction({ accounts, user }) {
 
 	const [currency, setCurrency] = useState('EUR');
 	const [targetUser, setTargetUser] = useState(recipient);
+	const [transferAmount, setTransferAmount] = useState('');
 
 	function handleChange(e) {
 		setCurrency(e.target.value);
@@ -36,6 +37,16 @@ export default function BasicCardTransaction({ accounts, user }) {
 	function handleUser(e) {
 		if (e.target.value === user) return;
 		setTargetUser(e.target.value);
+	}
+
+	function handleTransferAmount(e) {
+		e.preventDefault();
+		setTransferAmount(e.target.value);
+	}
+
+	function handleReturn(e) {
+		e.preventDefault();
+		return;
 	}
 
 	return (
@@ -108,6 +119,7 @@ export default function BasicCardTransaction({ accounts, user }) {
 									select
 									label="Select"
 									defaultValue={recipient[0]}
+									// value={recipient}
 									helperText="Select reciever"
 									onChange={handleUser}
 								>
@@ -123,29 +135,40 @@ export default function BasicCardTransaction({ accounts, user }) {
 						{/* //! Amount */}
 
 						<Box
-							component="form"
 							sx={{
 								'& .MuiTextField-root': { m: 1, width: '25ch' },
 							}}
 							noValidate
 							autoComplete="off"
 						>
-							<TextField
-								id="outlined-select-currency"
-								// select
-								label="amount"
-								defaultValue=""
-								helperText="Please select amount"
-							>
-								<input
-									type="number"
-									// className={classes.form__input}
-									// onChange={(e) => setTransferRecipient(e.target.value)}
-									// value={transferRecipient}
-								/>
-							</TextField>
+							<form component="form" onSubmit={handleReturn}>
+								<TextField
+									onChange={handleTransferAmount}
+									id="outlined-select-currency"
+									// select
+									label="amount"
+									// defaultValue={transferAmount}
+									helperText="Please select amount"
+								></TextField>
+								{/* <input onSubmit={() => handleReturn}></input> */}
+							</form>
 						</Box>
 					</Box>
+				</Box>
+				<Box>
+					<Button
+						sx={{
+							'&:hover': {
+								backgroundColor: 'black',
+								cursor: 'default',
+							},
+							bgcolor: '#585054',
+							color: 'white',
+							mt: 2,
+						}}
+					>
+						Transfer
+					</Button>
 				</Box>
 			</CardContent>
 		</Card>
