c9242807d8f68f00156431c89609e3030d9db53e
Dazboy2020 <darrendowlut@tutanota.com>
2023-08-10 11:16:44 +0200

    initial responsiveness added to movements

--- src/components/Movements/Movements.js
+++ src/components/Movements/Movements.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Grid, Stack } from '@mui/material';
 import classes from './Movements.module.css';
 
 const Movements = ({ accountMovements, currency, sort }) => {
@@ -13,40 +13,61 @@ const Movements = ({ accountMovements, currency, sort }) => {
 		: movementsToDisplay;
 
 	return (
-		<div className={classes.main__container__window}>
-			<ul>
-				{moves.map((item) => (
-					<div
-						key={Math.floor(Math.random() * 10000) + 1}
-						className={classes.movements}
-					>
-						{item[0] > 0 && (
-							<div className={classes.movements__row}>
-								<div className={classes.movements__type__deposit}>deposit</div>
-								<div className={classes.movements__date}>{item[1]}</div>
-								<div className={classes.movements__value}>
-									{item[0].toFixed(2)}
-									{currency === 'euro' ? '€' : '$'}
-								</div>
-							</div>
-						)}
+		// <Grid container sx={{ flexGrow: 1 }}>
+		<Stack>
+			{moves.map((item) => (
+				<Grid
+					container
+					key={Math.floor(Math.random() * 10000) + 1}
+					className={classes.movements}
+				>
+					{item[0] > 0 && (
+						// <div className={classes.movements__row}>
+						<>
+							<Grid
+								item
+								md={3}
+								sm={4}
+								className={classes.movements__type__deposit}
+							>
+								deposit
+							</Grid>
+							<Grid item md={9} sm={4} className={classes.movements__date}>
+								{item[1]}
+							</Grid>
+							<Grid item md={9} sm={4} className={classes.movements__value}>
+								{item[0].toFixed(2)}
+								{currency === 'euro' ? '€' : '$'}
+							</Grid>
+						</>
+						// </div>
+					)}
 
-						{item[0] < 1 && (
-							<div className={classes.movements__row}>
-								<div className={classes.movements__type__withdrawal}>
-									withdrawal
-								</div>
-								<div className={classes.movements__date}>{item[1]}</div>
-								<div className={classes.movements__value}>
-									{item[0].toFixed(2)}
-									{currency === 'euro' ? '€' : '$'}
-								</div>
-							</div>
-						)}
-					</div>
-				))}
-			</ul>
-		</div>
+					{item[0] < 1 && (
+						// <div className={classes.movements__row}>
+						<>
+							<Grid
+								item
+								md={3}
+								sm={4}
+								className={classes.movements__type__withdrawal}
+							>
+								withdrawal
+							</Grid>
+							<Grid item md={9} sm={4} className={classes.movements__date}>
+								{item[1]}
+							</Grid>
+							<Grid item md={9} sm={4} className={classes.movements__value}>
+								{item[0].toFixed(2)}
+								{currency === 'euro' ? '€' : '$'}
+							</Grid>
+						</>
+						// </div>
+					)}
+				</Grid>
+			))}
+		</Stack>
+		// </Grid>
 	);
 };
 
