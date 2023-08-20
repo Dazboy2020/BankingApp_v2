69a481aec6b9b00af881cbfe551f2bd81474e0bd
Dazboy2020 <darrendowlut@tutanota.com>
2023-08-20 14:42:46 +0200

    expenses category now added to movements list

--- src/components/Outline-Card/OutlinedCardFX.js
+++ src/components/Outline-Card/OutlinedCardFX.js
@@ -105,6 +105,7 @@ const BasicCardFX = ({
 			updatedExpenses.unshift([
 				-expenseAmount,
 				new Date().toLocaleDateString(),
+				expenseCategory,
 			]);
 		}
 
@@ -254,7 +255,7 @@ const BasicCardFX = ({
 						}}
 						onClick={handleSubmitExpense}
 					>
-						Add Transaction
+						Add Item
 					</Button>
 				</Box>
 			</CardContent>
