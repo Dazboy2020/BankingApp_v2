80b8f818b386f1e08d2fdea0189405cd7dbc5632
Dazboy2020 <darrendowlut@tutanota.com>
2023-08-23 01:21:21 +0200

    theme

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
--- src/pages/MainApp.js
+++ src/pages/MainApp.js
@@ -13,6 +13,14 @@ import PieEuro from '../components/Charts/PieEuro';
 // import classes from '../components/Charts/pie_wrapper.Module.css';
 import styles from './MainApp.module.css';
 import MovementsExpenses from '../components/Movements/Movements_Expenses';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline, createTheme } from '@mui/material';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
 
 function MainApp({
 	open,
@@ -34,57 +42,59 @@ function MainApp({
 	sort,
 }) {
 	return (
-		<div className={styles.body}>
-			<>
-				<AlertDialogSlide
-					open={open}
-					setOpen={setOpen}
-					LogUserOut={LogUserOut}
-				/>
-				<CloseAccountModal
-					openModal={openModal}
-					setOpenModal={setOpenModal}
-					LogUserOut={LogUserOut}
-				/>
-				<Toast openToast={openToast} setOpenToast={setOpenToast} />
-				<Stack
-					direction={{ s: 'column', sm: 'row' }}
-					sx={{
-						justifyContent: 'space-between',
-						// m: 1,
-					}}
-				>
-					<BasicCardSummary
-						sx={{
-							width: {
-								lg: 200,
-							},
-						}}
-						accountMovements={accountMovements}
-						totalExpenses={totalExpenses}
-						totalIncome={totalIncome}
-						currency={currency}
-						setSort={setSort}
-						accounts={accounts}
-						user={user}
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<div className={styles.body}>
+				<>
+					<AlertDialogSlide
+						open={open}
+						setOpen={setOpen}
+						LogUserOut={LogUserOut}
 					/>
-					<BasicCardFX
+					<CloseAccountModal
+						openModal={openModal}
+						setOpenModal={setOpenModal}
+						LogUserOut={LogUserOut}
+					/>
+					<Toast openToast={openToast} setOpenToast={setOpenToast} />
+					<Stack
+						direction={{ s: 'column', sm: 'row' }}
 						sx={{
-							width: {
-								lg: 200,
-							},
+							justifyContent: 'space-between',
+							// m: 1,
 						}}
-						accountMovements={accountMovements}
-						setAccountMovements={setAccountMovements}
-						setOpenToast={setOpenToast}
+					>
+						<BasicCardSummary
+							sx={{
+								width: {
+									lg: 200,
+								},
+							}}
+							accountMovements={accountMovements}
+							totalExpenses={totalExpenses}
+							totalIncome={totalIncome}
+							currency={currency}
+							setSort={setSort}
+							accounts={accounts}
+							user={user}
+						/>
+						<BasicCardFX
+							sx={{
+								width: {
+									lg: 200,
+								},
+							}}
+							accountMovements={accountMovements}
+							setAccountMovements={setAccountMovements}
+							setOpenToast={setOpenToast}
 
-						// totalExpenses={totalExpenses}
-						// totalIncome={totalIncome}
-						// setSort={setSort}
-						// accounts={accoun
-						// user={user}
-					/>
-					{/* <BasicCardTransaction
+							// totalExpenses={totalExpenses}
+							// totalIncome={totalIncome}
+							// setSort={setSort}
+							// accounts={accoun
+							// user={user}
+						/>
+						{/* <BasicCardTransaction
 							accountMovements={accountMovements}
 							setAccountMovements={setAccountMovements}
 							currency={currency}
@@ -95,47 +105,48 @@ function MainApp({
 							user={user}
 							setOpenToast={setOpenToast}
 						/>  */}
-				</Stack>
-				<Stack
-					className={styles.pie_wrapper}
-					direction={{ xs: 'column', md: 'row' }}
-					sx={{
-						display: 'flex',
-						backgroundColor: '#EEEEEE',
-						alignItems: 'centre',
-						justifyContent: 'space-around',
-						paddingTop: '1rem',
-						// height: '40vh',
-					}}
-				>
-					<PieChart
-						accountMovements={accountMovements}
-						currency={currency}
-						sort={sort}
-						setSort={setSort}
-					/>
-					<PieEuro
-						accountMovements={accountMovements}
-						currency={currency}
-						sort={sort}
-					/>
-				</Stack>
-				<MovementList>
-					<Stack direction={{ xs: 'column', md: 'row' }} sx={{ flexGrow: 1 }}>
-						<Movements
+					</Stack>
+					<Stack
+						className={styles.pie_wrapper}
+						direction={{ xs: 'column', md: 'row' }}
+						sx={{
+							display: 'flex',
+							backgroundColor: '#EEEEEE',
+							alignItems: 'centre',
+							justifyContent: 'space-around',
+							paddingTop: '1rem',
+							// height: '40vh',
+						}}
+					>
+						<PieChart
 							accountMovements={accountMovements}
-							// currency={currency}
+							currency={currency}
 							sort={sort}
+							setSort={setSort}
 						/>
-						<MovementsExpenses
+						<PieEuro
 							accountMovements={accountMovements}
-							// currency={currency}
+							currency={currency}
 							sort={sort}
 						/>
 					</Stack>
-				</MovementList>
-			</>
-		</div>
+					<MovementList>
+						<Stack direction={{ xs: 'column', md: 'row' }} sx={{ flexGrow: 1 }}>
+							<Movements
+								accountMovements={accountMovements}
+								// currency={currency}
+								sort={sort}
+							/>
+							<MovementsExpenses
+								accountMovements={accountMovements}
+								// currency={currency}
+								sort={sort}
+							/>
+						</Stack>
+					</MovementList>
+				</>
+			</div>
+		</ThemeProvider>
 	);
 }
 
