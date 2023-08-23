14e5e4c5f418e22f810f119cb0e1f7f961e3561b
Dazboy2020 <darrendowlut@tutanota.com>
2023-08-23 10:59:06 +0200

    fixed styling and responsiveness for background image

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -76,94 +76,94 @@ export default function SignIn({
 		<>
 			<NewNav />
 			<Box className={classes.wrapper}>
-				{/* <ThemeProvider theme={defaultTheme}> */}
-				<Container component="main" maxWidth="xs">
-					<CssBaseline />
-					<Box
-						sx={{
-							backgroundColor: 'white',
-							marginTop: 8,
-							display: 'flex',
-							flexDirection: 'column',
-							alignItems: 'center',
-							border: 'solid 1px black',
-							padding: '3rem',
-							boxShadow:
-								'0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-						}}
-					>
-						<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-							<LockOutlinedIcon />
-						</Avatar>
-						<Typography component="h1" variant="h5">
-							Sign in
-						</Typography>
-						<Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-							<TextField
-								margin="normal"
-								required
-								fullWidth
-								id="username"
-								label="username"
-								name="username"
-								autoComplete="username"
-								autoFocus
-								value={user}
-								onChange={(e) => setUser(e.target.value)}
-								onBlur={onBlurHandler}
-								color="secondary"
-							/>
-							<TextField
-								margin="normal"
-								required
-								fullWidth
-								name="pin"
-								label="pin"
-								type="password"
-								id="pin"
-								autoComplete="current-pin"
-								value={pin}
-								onChange={(e) => setPin(e.target.value)}
-								onBlur={onBlurHandler}
-								color="secondary"
-							/>
-							{error ? (
-								<p style={{ color: 'red' }}>Something went wrong!</p>
-							) : (
-								<p style={{ color: 'white' }}>Something went wrong!</p>
-							)}
-							<FormControlLabel
-								sx={{ mt: 5 }}
-								control={<Checkbox value="remember" color="primary" />}
-								label="Remember me"
-							/>
-							<Button
-								startIcon={<Login size="medium" />}
-								type="submit"
-								fullWidth
-								variant="contained"
-								sx={{ mt: 1, mb: 2 }}
-								color="secondary"
-							>
-								Sign In
-							</Button>
-							<Grid container>
-								<Grid item xs>
-									<Link href="#" variant="body2">
-										Forgot password?
-									</Link>
+				<ThemeProvider theme={defaultTheme}>
+					<Container component="main" maxWidth="xs">
+						<CssBaseline />
+						<Box
+							sx={{
+								backgroundColor: 'white',
+								marginTop: 8,
+								display: 'flex',
+								flexDirection: 'column',
+								alignItems: 'center',
+								border: 'solid 1px black',
+								padding: '3rem',
+								boxShadow:
+									'0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+							}}
+						>
+							<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+								<LockOutlinedIcon />
+							</Avatar>
+							<Typography component="h1" variant="h5">
+								Sign in
+							</Typography>
+							<Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+								<TextField
+									margin="normal"
+									required
+									fullWidth
+									id="username"
+									label="username"
+									name="username"
+									autoComplete="username"
+									autoFocus
+									value={user}
+									onChange={(e) => setUser(e.target.value)}
+									onBlur={onBlurHandler}
+									color="secondary"
+								/>
+								<TextField
+									margin="normal"
+									required
+									fullWidth
+									name="pin"
+									label="pin"
+									type="password"
+									id="pin"
+									autoComplete="current-pin"
+									value={pin}
+									onChange={(e) => setPin(e.target.value)}
+									onBlur={onBlurHandler}
+									color="secondary"
+								/>
+								{error ? (
+									<p style={{ color: 'red' }}>Something went wrong!</p>
+								) : (
+									<p style={{ color: 'white' }}>Something went wrong!</p>
+								)}
+								<FormControlLabel
+									sx={{ mt: 5 }}
+									control={<Checkbox value="remember" color="primary" />}
+									label="Remember me"
+								/>
+								<Button
+									startIcon={<Login size="medium" />}
+									type="submit"
+									fullWidth
+									variant="contained"
+									sx={{ mt: 1, mb: 2 }}
+									color="secondary"
+								>
+									Sign In
+								</Button>
+								<Grid container>
+									<Grid item xs>
+										<Link href="#" variant="body2">
+											Forgot password?
+										</Link>
+									</Grid>
+									<Grid item>
+										<NavLink to="/signup" variant="body2">
+											{"Don't have an account? Sign Up"}
+										</NavLink>
+									</Grid>
 								</Grid>
-								<Grid item>
-									<NavLink to="/signup" variant="body2">
-										{"Don't have an account? Sign Up"}
-									</NavLink>
-								</Grid>
-							</Grid>
+							</Box>
 						</Box>
-					</Box>
-					<Copyright sx={{ mt: 8, mb: 4 }} />
-				</Container>
-				{/* </ThemeProvider> */}
+						<Copyright sx={{ mt: 8, mb: 4 }} />
+					</Container>
+				</ThemeProvider>
 			</Box>
 		</>
 	);
