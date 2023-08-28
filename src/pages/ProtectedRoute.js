b7c0eca3b025ccece5d204f7f62c95ceb4bf182e
Dazboy2020 <darrendowlut@tutanota.com>
2023-08-29 00:49:53 +0200

    protected route redirecting to signin page. TODO: onBlur

--- src/pages/ProtectedRoute.js
+++ src/pages/ProtectedRoute.js
@@ -2,20 +2,21 @@ import { useNavigate } from 'react-router-dom';
 import { useAppContext } from '../context/context';
 import { useEffect } from 'react';
 
+import SignIn from '../pages/SignIn';
+
 function ProtectedRoute({ children }) {
 	const { state } = useAppContext();
-	console.log(state);
 
 	const navigate = useNavigate();
 
 	useEffect(
 		function () {
-			if (!state.user) navigate('/');
+			if (!state.user) navigate('/login');
 		},
 		[state.user, navigate]
 	);
 
-	return state.user ? children : null;
+	return state.user ? children : <SignIn />;
 }
 
 export default ProtectedRoute;
