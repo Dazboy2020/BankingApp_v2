f7feed2255bfadeaff7215ae8b8b2f1deac91689
Dazboy2020 <darrendowlut@tutanota.com>
2023-08-30 13:10:49 +0200

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 import ResponsiveAppBar from './Navbar/NewNav';
 import Homepage from './components/Homepage/Homepage';
-import MainApp from './pages/MainApp';
+import { lazy } from 'react';
+import SpinnerFullPage from './components/Spinner/SpinnerFullPage';
 
+// import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
+// import MainApp from './pages/MainApp';
 import ProtectedRoute from './pages/ProtectedRoute';
+
+const SignIn = lazy(() => import('./pages/SignIn'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+const MainApp = lazy(() => import('./pages/MainApp'));
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route
-					index
-					element={
-						<>
-							<ResponsiveAppBar />
-							<Homepage />
-						</>
-					}
-				/>
-				<Route
-					path="application"
-					element={
-						<ProtectedRoute>
-							<ResponsiveAppBar />
-							<MainApp />
-						</ProtectedRoute>
-					}
-				/>
-				<Route path="login" element={<SignIn />} />
-				<Route path="signup" element={<SignUp />} />
-			</Routes>
+			<Suspense fallback={<SpinnerFullPage />}>
+				<Routes>
+					<Route
+						index
+						element={
+							<>
+								<ResponsiveAppBar />
+								<Homepage />
+							</>
+						}
+					/>
+					<Route
+						path="application"
+						element={
+							<ProtectedRoute>
+								<ResponsiveAppBar />
+								<MainApp />
+							</ProtectedRoute>
+						}
+					/>
+					<Route path="login" element={<SignIn />} />
+					<Route path="signup" element={<SignUp />} />
+				</Routes>
+			</Suspense>
 		</BrowserRouter>
 	);
 }
