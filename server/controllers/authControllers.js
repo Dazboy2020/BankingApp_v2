37b36a8ae0f2f7cd999da5a75eca712a6f860a45
Dazboy2020 <darrendowlut@tutanota.com>
2023-09-06 17:15:23 +0200

    added dynamic toast messages

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,11 +1,42 @@
+const User = require('../models/user');
+
 const test = (req, res) => {
 	res.json('test is working!');
 };
 
-const registerUser = (req, res) => {
+const registerUser = async (req, res) => {
 	try {
 		const { name, email, password } = req.body;
-	} catch (error) {}
+
+		if (!name) {
+			return res.json({
+				error: 'Name is required!',
+			});
+		}
+
+		if (!password || password.length < 6) {
+			return res.json({
+				error: 'Password is required and should be at least 6 characters long!',
+			});
+		}
+
+		const exist = await User.findOne({ email });
+		if (exist) {
+			return res.json({
+				error: 'Email already exists',
+			});
+		}
+
+		const user = await User.create({
+			name,
+			email,
+			password,
+		});
+
+		return res.json(user);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = {
