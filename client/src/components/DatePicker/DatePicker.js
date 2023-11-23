752229ca76e4e70fa532602f372678da7b46e396
Dazboy2020 <darrendowlut@tutanota.com>
2023-11-23 09:07:53 +0100

    bug fix: page no longer crashed when date selected and page change

--- client/src/components/DatePicker/DatePicker.js
+++ client/src/components/DatePicker/DatePicker.js
@@ -2,9 +2,50 @@ import * as React from 'react';
 import dayjs from 'dayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { useTransactionContext } from '../../context/transactionContext';
+import { useDarkMode } from '../../Hooks/useDarkMode';
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
 
 export default function DatePickerValue() {
-	const { pickerDate, setPickerDate } = useTransactionContext();
+	const { isDarkMode } = useDarkMode();
+
+	// const defaultMaterialTheme = createTheme({
+	// 	MuiTypography: {
+	// 		styleOverrides: {
+	// 			root: {
+	// 				color: isDarkMode ? '#d6d3d1' : '#000',
+	// 			},
+	// 		},
+	// 	},
+	// });
+
+	const pickerStyle = {
+		'& .MuiInputBase-root': {
+			color: isDarkMode ? '#d6d3d1' : '#000',
+			borderColor: isDarkMode ? '#d6d3d1' : '#000',
+		},
+		'& .MuiPaper-root': {
+			color: isDarkMode ? '#d6d3d1' : '#000',
+			borderColor: isDarkMode ? '#d6d3d1' : '#000',
+		},
+		'& .MuiSvgIcon-root': {
+			color: isDarkMode ? '#d6d3d1' : '#000',
+		},
+
+		'& .MuiDateCalendar-root': {
+			color: isDarkMode ? '#d6d3d1' : '#000',
+		},
+		'& .MuiPickersCalendarHeader-root': {
+			color: 'white',
+		},
+
+		width: '20ch',
+		m: 1,
+		color: isDarkMode ? '#d6d3d1' : '#000',
+	};
+
+	const { pickerDate, setPickerDate, expenseAmount, expenseCategory } =
+		useTransactionContext();
 
 	const formatDateToString = (date) => {
 		return date ? dayjs(date).format('DD MMM YYYY') : ''; // Check if date is defined
@@ -15,19 +56,80 @@ export default function DatePickerValue() {
 
 	React.useEffect(
 		function () {
-			setPickerDate(formattedDate);
+			if (expenseAmount && expenseCategory) setPickerDate(formattedDate);
 		},
-		[formattedDate, setPickerDate]
+		[formattedDate, setPickerDate, expenseAmount, expenseCategory]
 	);
 
+	const datePickerSlotProps = {
+		day: {
+			sx: {
+				'& .MuiPickersCalendarHeader-root': {
+					color: 'white',
+				},
+				'&.MuiPickersDay-root.Mui-selected': {
+					backgroundColor: isDarkMode ? '#d6d3d1' : '#000',
+				},
+				':not(.Mui-selected)': {
+					backgroundColor: '#fff',
+					borderColor: isDarkMode ? '#d6d3d1' : '#000',
+				},
+				'&:hover': {
+					backgroundColor: isDarkMode ? '#d6d3d1' : '#000',
+					borderColor: isDarkMode ? '#d6d3d1' : '#000',
+					color: 'white',
+					transition: 'all 0.5s ease',
+				},
+				'&.Mui-selected': {
+					color: '#fff',
+					backgroundColor: isDarkMode ? '#d6d3d1' : '#000',
+					borderColor: isDarkMode ? '#d6d3d1' : '#000',
+					'&:hover': {
+						color: '#fff',
+						backgroundColor: isDarkMode ? '#d6d3d1' : '#000',
+						borderColor: isDarkMode ? '#d6d3d1' : '#000',
+						transition: 'all 0.5s ease',
+					},
+				},
+			},
+		},
+		desktopPaper: {
+			sx: {
+				'.MuiPickersYear-yearButton.Mui-selected': {
+					color: '#fff',
+					backgroundColor: isDarkMode ? '#d6d3d1' : '#000',
+				},
+				'.MuiPickersMonth-monthButton.Mui-selected': {
+					color: '#fff',
+					backgroundColor: isDarkMode ? '#d6d3d1' : '#000',
+				},
+			},
+		},
+		mobilePaper: {
+			sx: {
+				'.MuiPickersYear-yearButton.Mui-selected': {
+					color: '#fff',
+					backgroundColor: isDarkMode ? '#d6d3d1' : '#000',
+				},
+				'.MuiPickersMonth-monthButton.Mui-selected': {
+					color: '#fff',
+					backgroundColor: isDarkMode ? '#d6d3d1' : '#000',
+				},
+			},
+		},
+	};
+
 	return (
+		// <ThemeProvider theme={defaultMaterialTheme}>
 		<DatePicker
-			sx={{
-				width: '20ch',
-			}}
+			sx={pickerStyle}
 			label="Select date"
 			value={pickerDate}
 			onChange={(newValue) => setPickerDate(newValue)}
+			slotProps={{
+				...datePickerSlotProps,
+			}}
 		/>
+		// </ThemeProvider>
 	);
 }
