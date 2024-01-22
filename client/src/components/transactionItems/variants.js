e96f4a7306541f5b2c204382140a95fe9a0ba254
Dazboy2020 <darrendowlut@tutanota.com>
2024-01-22 20:48:23 +0100

    moved animation logic for transactions into movements.module file

--- client/src/components/transactionItems/variants.js
+++ client/src/components/transactionItems/variants.js
@@ -1,13 +1,12 @@
 export const containerVariants = {
 	hidden: {
-		height: 0,
-		opacity: 0,
+		// height: 0,
+		// opacity: 1,
 	},
 	visible: (index) => ({
-		height: 'auto',
+		// height: 'auto',
 		opacity: 1,
 		// scale: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-
 		transition: {
 			opacity: { delay: 0.025 },
 		},
