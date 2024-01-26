012786762e73a047bd8f490580267c65f788f31f
Dazboy2020 <darrendowlut@tutanota.com>
2024-01-26 20:07:21 +0100

    Transaction styling rendered with sx prop

--- client/src/components/transactionItems/deposit-card/DepositCardTitle.js
+++ client/src/components/transactionItems/deposit-card/DepositCardTitle.js
@@ -1,18 +1,27 @@
 import { useDarkMode } from '../../../hooks/useDarkMode';
-import { Box } from '@mui/material';
-import classes from '../layout/Movements.module.css';
+import { Box, Typography } from '@mui/material';
 import NorthEastIcon from '@mui/icons-material/NorthEast';
 
 function DepositCardTitle() {
 	const { isDarkMode } = useDarkMode();
+
+	const titleStyling = {
+		backgroundColor: isDarkMode ? '#212529' : '#495057',
+		fontSize: '1.2rem',
+		textTransform: 'uppercase',
+		letterSpacing: '0.1rem',
+		color: ' #fff',
+		padding: '0.5rem',
+		width: '15rem',
+		mb: '0.1rem',
+		borderRadius: '8px',
+		fontWeight: 600,
+	};
+
 	return (
 		<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-			<span
-				className={
-					isDarkMode ? classes.movements__type__dark : classes.movements__type
-				}
-			>
-				Income
+			<span>
+				<Typography sx={titleStyling}>Income</Typography>
 			</span>
 			<NorthEastIcon sx={{ fontSize: '40px', color: 'green' }} />
 		</Box>
--- client/src/components/transactionItems/expense-card/ExpenseCardTitle.js
+++ client/src/components/transactionItems/expense-card/ExpenseCardTitle.js
@@ -1,19 +1,27 @@
 import { useDarkMode } from '../../../hooks/useDarkMode';
-import { Box } from '@mui/material';
-import classes from '../layout/Movements.module.css';
+import { Box, Typography } from '@mui/material';
 import SouthEastIcon from '@mui/icons-material/SouthEast';
 
 function ExpenseCardTitle() {
 	const { isDarkMode } = useDarkMode();
 
+	const titleStyling = {
+		backgroundColor: isDarkMode ? '#212529' : '#495057',
+		fontSize: '1.2rem',
+		textTransform: 'uppercase',
+		letterSpacing: '0.1rem',
+		color: ' #fff',
+		padding: '0.5rem',
+		width: '15rem',
+		mb: '0.1rem',
+		borderRadius: '8px',
+		fontWeight: 600,
+	};
+
 	return (
 		<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-			<span
-				className={
-					isDarkMode ? classes.movements__type__dark : classes.movements__type
-				}
-			>
-				Expense
+			<span>
+				<Typography sx={titleStyling}>Expense</Typography>
 			</span>
 			<SouthEastIcon sx={{ fontSize: '40px', color: 'red' }} />
 		</Box>
