e48aa7209d5fea5cbf9634203e791722ffd8741b
Dazboy2020 <darrendowlut@tutanota.com>
2024-01-27 21:58:26 +0100

    removed mainapp.module.css and moved to sx prop

--- client/src/pages/layout/PageLayout.js
+++ client/src/pages/layout/PageLayout.js
@@ -1,17 +1,18 @@
 import { useDarkMode } from '../../hooks/useDarkMode';
 import { Box } from '@mui/material';
-import classes from '../MainApp.module.css';
 
 function PageLayout({ children }) {
 	const { isDarkMode } = useDarkMode();
 
 	return (
 		<Box
-			className={isDarkMode ? classes.darkmode : classes.body}
 			sx={{
 				minHeight: '100vh',
 				ml: { lg: '22rem', md: '22rem', sm: '19rem', xs: 0 },
 				mr: { lg: 0, sm: 0, m: 0 },
+				backgroundColor: isDarkMode ? ' #212529' : '#343a40',
+				overflowX: 'hidden',
+				overflowY: 'hidden',
 			}}
 		>
 			<Box
