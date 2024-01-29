1fd1ae3a20181f7f5a8dfc01bce713d33a3b3114
Dazboy2020 <darrendowlut@tutanota.com>
2024-01-29 20:21:42 +0100

    Refactored Budget page. Removed TransactionLayout Component

--- client/src/pages/Budget.js
+++ client/src/pages/Budget.js
@@ -1,15 +1,50 @@
-import TransactionLayout from './layout/TransactionLayout';
+import { useAppContext } from '../context/context';
+import { useTransactionContext } from '../context/transactionContext';
 import TotalExpensesCard from '../components/summary-cards/TotalExpensesCard';
 import CombinedItems from '../components/transactionItems/CombinedItems';
+import ResponsiveDrawer from '../components/drawer/Draw';
+import PageLayout from './layout/PageLayout';
+import SummaryCardSection from '../features/summaryCard-section/SummaryCardSection';
+import BudgetCard from '../components/summary-cards/BudgetCard';
+import AddTransaction from '../components/summary-cards/AddTransaction';
+import AvailbleFundsCard from '../components/summary-cards/AvailableFundsCard';
+import useDisplayFilterComponent from '../hooks/useDisplayFilterComponent';
+import { Box } from '@mui/material';
 
-function Budget({ children }) {
+function Budget() {
+	const { state } = useAppContext();
+	const { expenseType } = useTransactionContext();
+
+	const { displayFilterComponent } = useDisplayFilterComponent();
+
+	function budgetCardToDisplay() {
+		if (state.isActive === 4 && !state.isEditing) {
+			return <BudgetCard />;
+		} else {
+			return <AddTransaction expenseType={expenseType} />;
+		}
+	}
+
+	const layout = {
+		mt: { xs: 5, md: 10 },
+	};
+
 	return (
-		<TransactionLayout
-			TransactionTypeCard={<TotalExpensesCard type="budget" />}
-			TransactionItems={<CombinedItems type="budget" />}
-		>
-			{children}
-		</TransactionLayout>
+		<>
+			<ResponsiveDrawer />
+			<PageLayout>
+				<SummaryCardSection
+					TotalExpensesCard={<TotalExpensesCard type="budget" />}
+					AvailbleFundsCard={<AvailbleFundsCard type="budget" />}
+					AddTransaction={budgetCardToDisplay()}
+				/>
+
+				<Box sx={layout}>
+					{displayFilterComponent()}
+					<CombinedItems type="budget" />
+				</Box>
+			</PageLayout>
+		</>
 	);
 }
 
