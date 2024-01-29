8ee3677b683d20e13d32047f21fb70c1a139f84b
Dazboy2020 <darrendowlut@tutanota.com>
2024-01-29 10:07:05 +0100

    added classed.movements wrapping div to animated list

--- client/src/components/animated-list/AnimatedList.js
+++ client/src/components/animated-list/AnimatedList.js
@@ -1,24 +1,26 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { containerVariants } from '../transactionItems/variants';
-
+import classes from '../transactionItems/layout/Movements.module.css';
 function AnimatedList({ items, children }) {
 	return (
-		<AnimatePresence>
-			{items.map((item, index) => (
-				<motion.li
-					style={{ listStyleType: 'none' }}
-					key={`${item.id}-${index}`}
-					custom={index}
-					initial="initial"
-					animate="animate"
-					exit="exitAnimation"
-					variants={containerVariants}
-					layout
-				>
-					{children(item)}
-				</motion.li>
-			))}
-		</AnimatePresence>
+		<div className={classes.movements__row}>
+			<AnimatePresence>
+				{items.map((item, index) => (
+					<motion.li
+						style={{ listStyleType: 'none' }}
+						key={`${item.id}-${index}`}
+						custom={index}
+						initial="initial"
+						animate="animate"
+						exit="exitAnimation"
+						variants={containerVariants}
+						layout
+					>
+						{children(item)}
+					</motion.li>
+				))}
+			</AnimatePresence>
+		</div>
 	);
 }
 
