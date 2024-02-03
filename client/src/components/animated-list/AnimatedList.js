d6e94a6fdf47f554481567a4f6748f8ec3c0f8c0
Dazboy2020 <darrendowlut@tutanota.com>
2024-02-03 23:08:06 +0100

    fixed page transition glitch by animating PageLayout wrapper

--- client/src/components/animated-list/AnimatedList.js
+++ client/src/components/animated-list/AnimatedList.js
@@ -4,25 +4,23 @@ import { Box } from '@mui/material';
 
 function AnimatedList({ items, children }) {
 	return (
-		<>
-			<AnimatePresence key="list-component">
-				{items.map((item, index) => (
-					<Box
-						component={motion.li}
-						style={{ listStyleType: 'none' }}
-						key={`${item.id}-${index}`}
-						custom={index}
-						initial="initial"
-						animate="animate"
-						exit={{ opacity: 0, scale: 0, height: 0 }}
-						variants={containerVariants}
-						layout
-					>
-						{children(item)}
-					</Box>
-				))}
-			</AnimatePresence>
-		</>
+		<AnimatePresence key="list-component">
+			{items.map((item, index) => (
+				<Box
+					component={motion.li}
+					style={{ listStyleType: 'none' }}
+					key={`${item.id}-${index}`}
+					custom={index}
+					initial="initial"
+					animate="animate"
+					exit={{ opacity: 0, scale: 0, height: 0 }}
+					variants={containerVariants}
+					layout
+				>
+					{children(item)}
+				</Box>
+			))}
+		</AnimatePresence>
 	);
 }
 
--- client/src/pages/layout/PageLayout.js
+++ client/src/pages/layout/PageLayout.js
@@ -2,36 +2,59 @@ import { useDarkMode } from '../../hooks/useDarkMode';
 import { Box } from '@mui/material';
 import AlertDialogSlide from '../../UI/AlertDialogue/AlertDialogue';
 import ResponsiveDrawer from '../../components/drawer/Draw';
+import { AnimatePresence, easeInOut, motion } from 'framer-motion';
 
 function PageLayout({ children }) {
 	const { isDarkMode } = useDarkMode();
+
+	const containerVariants = {
+		initial: {
+			opacity: 0,
+		},
+		animate: (index) => ({
+			opacity: 1,
+			y: 0,
+			transition: {
+				opacity: { duration: 0.5, ease: easeInOut },
+			},
+		}),
+	};
 
 	return (
 		<>
 			<AlertDialogSlide />
 			<ResponsiveDrawer />
-			<Box
-				sx={{
-					minHeight: '100vh',
-					ml: { lg: '22rem', md: '22rem', sm: '19rem', xs: 0 },
-					mr: { lg: 0, sm: 0, m: 0 },
-					backgroundColor: isDarkMode ? ' #212529' : '#343a40',
-					overflowX: 'hidden',
-					overflowY: 'hidden',
-				}}
-			>
+			<AnimatePresence>
 				<Box
-					direction={{ sm: 'column', md: 'row' }}
+					key="pageLayout"
+					component={motion.div}
+					initial="initial"
+					animate="animate"
+					exit={{ y: '5%', opacity: 0 }}
+					variants={containerVariants}
+					// layout
 					sx={{
-						justifyContent: { sm: 'flex-start', md: 'center' },
-						ml: { xs: 3, sm: 6 },
-						mr: { xs: 3, sm: 6 },
-						mt: { xs: 5, md: 10 },
+						minHeight: '100vh',
+						ml: { lg: '22rem', md: '22rem', sm: '19rem', xs: 0 },
+						mr: { lg: 0, sm: 0, m: 0 },
+						backgroundColor: isDarkMode ? ' #212529' : '#343a40',
+						overflowX: 'hidden',
+						overflowY: 'hidden',
 					}}
 				>
-					{children}
+					<Box
+						direction={{ sm: 'column', md: 'row' }}
+						sx={{
+							justifyContent: { sm: 'flex-start', md: 'center' },
+							ml: { xs: 3, sm: 6 },
+							mr: { xs: 3, sm: 6 },
+							mt: { xs: 5, md: 10 },
+						}}
+					>
+						{children}
+					</Box>
 				</Box>
-			</Box>
+			</AnimatePresence>
 		</>
 	);
 }
